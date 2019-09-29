f056ee683792473add7fabea8706a4592c77b7b8
ZakariaAourir <zakaria1997aourir@gmail.com>
2019-09-29 19:30:52 +0100

    deploy

--- dist/route/index.js
+++ dist/route/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const router = express.Router()
 const nodemailer = require('nodemailer')
+const router = express.Router()
 require('dotenv').config()
 
 router.get('/', (req, res, next) => {
--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ const port = 3000
 const app = express()
 
 //routes
-const index = require('./route/index')
+const index = require('../dist/route/index')
 // view engine
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, '../dist/views'))
 app.set('view engine', 'pug')
 
 // bodyParser
@@ -22,8 +22,8 @@ app.use(bodyParser.json())
 // moment
 app.locals.moment = require('moment')
 // static folder
-app.set(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set(express.static(path.join(__dirname, '../dist/public')))
+app.use(express.static(path.join(__dirname, '../dist/public')))
 
 app.use('/', index)
 //running the port
